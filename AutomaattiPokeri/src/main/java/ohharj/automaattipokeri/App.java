3abde7a019ae1fc838ce63eab722f8c4092d5deb
MrNerd24 <juuso.lassila@helsinki.fi>
2015-01-13 16:36:59 +0200

    Projektin luonti

---
+++ AutomaattiPokeri/src/main/java/ohharj/automaattipokeri/App.java
@@ -0,0 +1,21 @@
+package ohharj.automaattipokeri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+    
+    /**
+     * HEllo
+     * 
+     */
+    public static void javaDocTest() {
+        
+    }
+}
